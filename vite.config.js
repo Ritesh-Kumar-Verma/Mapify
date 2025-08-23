0b5e35b9485d20b83f2e6e87ed7fbc60aa33e70a
Ritesh Kumar Verma <67832781+Ritesh-Kumar-Verma@users.noreply.github.com>
2025-08-23 10:52:23 +0530

    removed temp dev code snippets

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   
-  server: {
-      host: true, // 👈 allows access from local network (e.g., 192.168.x.x)
-      port: 5173, // or any port you want
-    },
-  
   base: '/Mapify/'
 })
 
+
+// server: {
+//     host: true, // 👈 allows access from local network (e.g., 192.168.x.x)
+//     port: 5173, // or any port you want
+//   },
 
